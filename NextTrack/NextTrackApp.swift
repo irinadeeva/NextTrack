bdf60b6919946201eb4ce3e6dc827be16e6f255e
irinadeeva <deeva97@me.com>
2024-12-07 07:34:21 +0500

    Initial Commit

---
+++ NextTrack/NextTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextTrackApp.swift
+//  NextTrack
+//
+//  Created by Irina Deeva on 07/12/24.
+//
+
+import SwiftUI
+
+@main
+struct NextTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
