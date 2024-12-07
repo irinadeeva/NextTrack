32e1d76be774bc7703f6bfdec5f02f8a17622551
irinadeeva <deeva97@me.com>
2024-12-07 20:17:54 +0300

    done

--- NextTrack/ContentView.swift
+++ NextTrack/ContentView.swift
@@ -8,17 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  @State var isNextTrack: Bool = false
+
+  var body: some View {
+    Button {
+      withAnimation(.interpolatingSpring(stiffness: 100, damping: 15)) {
+        isNextTrack.toggle()
+      } completion: {
+        isNextTrack.toggle()
+      }
+    } label: {
+      GeometryReader { proxy in
+        let size = proxy.size.width / 2
+
+        HStack(spacing: -1) {
+          buttonImage
+            .frame(width: isNextTrack ? size : 0)
+            .opacity(isNextTrack ? 1 : 0)
+
+          buttonImage
+            .frame(width: size)
+
+          buttonImage
+            .frame(width: isNextTrack ? 1 : size)
+            .opacity(isNextTrack ? 0 : 1)
         }
-        .padding()
+      }
     }
+    .frame(width: 50, height: 100)
+
+  }
+
+  var buttonImage: some View {
+    Image(systemName: "play.fill")
+      .resizable()
+      .scaledToFit()
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
